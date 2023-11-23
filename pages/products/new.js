6eef3f8a9148a84e88d5bf04ce9f2bc8e8f2cf53
Amir Begic <amir.begic12@gmail.com>
2023-11-23 03:30:06 +0100

    post method implemented

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,20 +1,53 @@
 import Layout from "@/components/Layout";
 import { useState } from "react";
+import axios from "axios";
+import { redirect } from "next/dist/server/api-utils";
+import { Router, useRouter } from "next/router";
 
-export default function NewProduct(){
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState('');
-    return (
-        <Layout>
-            <h1>New product</h1>
-            <label>Product name</label>
-            <input type="text" placeholder="product name"></input>
-            <label>Description</label>
-            <textarea placeholder="Description"></textarea>
-            <label>Price (u KM)</label>
-            <input type="number" placeholder="price"></input>
-            <button className="btn-primary">Save</button>
-        </Layout>
-    );
+export default function NewProduct() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [goToProducts, setGoToProducts] = useState(false);
+  const router = useRouter();
+
+  async function createProduct(ev) {
+    ev.preventDefault();
+    const data = { title, description, price };
+    await axios.post("/api/products", data);
+    setGoToProducts(true);
+  }
+  if (goToProducts) {
+     router.push('/products');
+  }
+  return (
+    <Layout>
+      <form onSubmit={createProduct}>
+        <h1>New product</h1>
+        <label>Product name</label>
+        <input
+          type="text"
+          placeholder="product name"
+          value={title}
+          onChange={(ev) => setTitle(ev.target.value)}
+        ></input>
+        <label>Description</label>
+        <textarea
+          placeholder="Description"
+          value={description}
+          onChange={(ev) => setDescription(ev.target.value)}
+        ></textarea>
+        <label>Price (u KM)</label>
+        <input
+          type="number"
+          placeholder="price"
+          value={price}
+          onChange={(ev) => setPrice(ev.target.value)}
+        ></input>
+        <button type="submit" className="btn-primary">
+          Save
+        </button>
+      </form>
+    </Layout>
+  );
 }
