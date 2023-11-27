35d341b6aec77ebd6f5963cf5b03311b960fb316
Amir Begic <amir.begic12@gmail.com>
2023-11-27 18:51:59 +0100

    fixed editing and adding delete page

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -3,28 +3,36 @@ import { useRouter } from "next/router";
 import axios from "axios";
 
 export default function ProductForm({
+  _id,
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
 }) {
-  const [title, setTitle] = useState(existingTitle || '');
-  const [description, setDescription] = useState(existingDescription || '');
-  const [price, setPrice] = useState(existingPrice || '');
+  const [title, setTitle] = useState(existingTitle || "");
+  const [description, setDescription] = useState(existingDescription || "");
+  const [price, setPrice] = useState(existingPrice || "");
   const [goToProducts, setGoToProducts] = useState(false);
   const router = useRouter();
 
-  async function createProduct(ev) {
+  async function saveProduct(ev) {
     ev.preventDefault();
     const data = { title, description, price };
-    await axios.post("/api/products", data);
+    if (_id) {
+      //update
+
+      await axios.put("/api/products", { ...data, _id });
+    } else {
+      //create
+
+      await axios.post("/api/products", data);
+    }
     setGoToProducts(true);
   }
   if (goToProducts) {
     router.push("/products");
   }
   return (
-    <form onSubmit={createProduct}>
-      
+    <form onSubmit={saveProduct}>
       <label>Product name</label>
       <input
         type="text"
---
+++ pages/products/delete/[...id].js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+export default function DeleteProductPage(){
+    reutrn (
+        <Layout>
+            Do you really want to delete product x?
+            <button>Yes</button>
+            <button>No</button>
+        </Layout>
+    );
+}
